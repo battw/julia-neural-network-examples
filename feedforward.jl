3f2a316cb8f7d20693693bbb89da4b1c3d2d9382
Ben Attwood <bendattwood@gmail.com>
2020-09-27 17:18:18 +0100

    implemented feedforward network initialisation

--- feedforward.jl
+++ feedforward.jl
@@ -8,6 +8,8 @@ using InteractiveUtils
 begin
 	using MLDatasets
 	using ImageCore
+	using OffsetArrays
+	using Images
 end
 
 # ╔═╡ 18e6adaa-000c-11eb-1244-bbd8a68d669d
@@ -20,7 +22,8 @@ md"## Imports"
 # ╔═╡ cbe8a3f6-0014-11eb-198b-f32ad4453e5e
 begin
 	import Pkg
-	Pkg.add(["MLDatasets", "ImageCore"])
+	Pkg.add(["MLDatasets", "ImageCore", "OffsetArrays"])
+	Pkg.add(["Images", "ImageIO", "ImageMagick"])
 end
 
 # ╔═╡ 474b29f0-000c-11eb-1f92-d9dd6686684a
@@ -61,18 +64,33 @@ md"## Variables"
 # ╔═╡ a210aa7a-00ca-11eb-3ff1-279e9ef75aea
 md"### Definitions
 - ``M`` - number of input examples
-- ``L`` - number of layers (includes the input as a layer)
-- ``N[l]`` - number of neurons in layer ``l``, with ``N[1]`` being the input size
-- ``X`` - input data in an ``N[1]\times M`` matrix
+- ``L`` - number of layers
+- ``N[l]`` - number of neurons in layer ``l``, for ``l\in(1:L)``
+- ``N[0]`` - number of features
+- ``X`` - input data in an ``N[0]\times M`` matrix
 - ``W`` - weight matrices of sizes ``N[l]\times N[l-1]`` in an array of length ``L`` 
-
 "
 
-# ╔═╡ 43b9c51a-002c-11eb-11f6-23b24e45c476
-X = flatten_images(train_x);
+# ╔═╡ 4bcf08a6-00d5-11eb-2d6f-29ae01e60198
+function network_init(data, layer_sizes)
+	M = size(data, 2)
+ 	L = length(layer_sizes)
+ 	N = OffsetArray([size(data, 1), layer_sizes...], 0:length(layer_sizes))
+ 	X = data
+ 	W = []
+	b = []
+	for l ∈ 1:L
+		push!(W, rand(N[l], N[l-1]))
+		push!(b, rand())
+	end
+	M, L, N, X, W, b
+end
 
-# ╔═╡ 87503cdc-002c-11eb-3c11-fb9632de122d
+# ╔═╡ cc224ea4-00d6-11eb-0b04-e940cc6374cf
+data = flatten_images(train_x);
 
+# ╔═╡ 55515a98-00d3-11eb-3340-031b9ea97166
+M, L, N, X, W, b = network_init(data, [30, 50, 10]);
 
 # ╔═╡ Cell order:
 # ╟─18e6adaa-000c-11eb-1244-bbd8a68d669d
@@ -84,6 +102,7 @@ X = flatten_images(train_x);
 # ╠═20eba704-000e-11eb-11eb-c7ef007de85d
 # ╠═21ff3b1c-0018-11eb-3a73-57b0a960523b
 # ╟─6adf3d48-002c-11eb-176e-7d6a52ceaffa
-# ╠═a210aa7a-00ca-11eb-3ff1-279e9ef75aea
-# ╠═43b9c51a-002c-11eb-11f6-23b24e45c476
-# ╠═87503cdc-002c-11eb-3c11-fb9632de122d
+# ╟─a210aa7a-00ca-11eb-3ff1-279e9ef75aea
+# ╠═4bcf08a6-00d5-11eb-2d6f-29ae01e60198
+# ╠═cc224ea4-00d6-11eb-0b04-e940cc6374cf
+# ╠═55515a98-00d3-11eb-3340-031b9ea97166
